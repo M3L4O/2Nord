9165b503ce79ed096c7e91e2fc3d80840f6a7713
João Victor Melo <joaovictor.slv@ufpi.edu.br>
2022-04-08 23:48:36 -0300

    rename function to best_color

--- twonord.py
+++ twonord.py
@@ -4,7 +4,7 @@ from sys import argv
 nord = [(46, 52, 64), (59, 66, 82), (67, 76, 94), (76, 86, 106), (216, 222, 233), (229, 233, 240), (236, 239, 244), (143, 188, 187), (136, 192, 208), (129, 161, 193), (94, 129, 172), (191, 97, 106), (208, 135, 112), (235, 203, 139), (163, 190, 140), (180, 142, 173)]
 
 
-def diff_in_color(pixel):
+def best_color(pixel):
     diff = []
     for color in nord:
         sum = abs(pixel[0] - color[0]) + abs(pixel[1] - color[1]) + abs(pixel[2] - color[2])
